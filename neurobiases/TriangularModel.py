added a couple utility functions. more tests on variance and cov2corr. fixed up the noise corr structure generation for non-target neurons. plotting for tuning curves has more robust functionality.

neurobiases/TriangularModel.py
@@ -1,5 +1,4 @@
 import h5py
-import matplotlib.pyplot as plt
 import neurobiases.utils as utils
 import numpy as np
 
@@ -67,9 +66,8 @@ class TriangularModel:
     """
     def __init__(
         self, model='linear', parameter_path=None, parameter_design='basis_functions',
-        n_latent=1, signal_to_noise=3.,
         coupling_kwargs=None, tuning_kwargs=None, stim_kwargs=None,
-        random_state=None
+        noise_kwargs=None, random_state=None
     ):
         self.model = model
         self.parameter_design = parameter_design
@@ -79,17 +77,20 @@ class TriangularModel:
             self.coupling_kwargs = coupling_kwargs
             self.tuning_kwargs = tuning_kwargs
             self.stim_kwargs = stim_kwargs
+            self.noise_kwargs = noise_kwargs
             self.N = self.coupling_kwargs.get('N', 100)
             self.M = self.tuning_kwargs.get('M', 10)
+            self.K = self.noise_kwargs.get('K', 2)
             self.coupling_kwargs['random_state'] = check_random_state(
                 coupling_kwargs.get('random_state', None)
             )
             self.tuning_kwargs['random_state'] = check_random_state(
                 tuning_kwargs.get('random_state', None)
             )
+            self.noise_kwargs['random_state'] = check_random_state(
+                noise_kwargs.get('random_state', None)
+            )
             self.random_state = check_random_state(random_state)
-            self.n_latent = n_latent
-            self.signal_to_noise = signal_to_noise
             # create parameters according to preferred design
             self.a, self.b, self.B = self.generate_triangular_model()
             self.generate_noise_structure()
@@ -218,24 +219,70 @@ class TriangularModel:
         return a, b, B
 
     def generate_noise_structure(self):
+        """Generates the noise covariance structure for the triangular model."""
+        # get noise correlation structure based off tuning preferences
+        noise_corr = utils.noise_correlation_matrix(
+            tuning_prefs=self.tuning_prefs[:-1],
+            corr_max=self.noise_kwargs.get('corr_max', 0.2),
+            corr_min=self.noise_kwargs.get('corr_min', -0.05),
+            L=self.noise_kwargs.get('L', 1.),
+            circular_stim=None)
         # calculate variance of linear inputs
         tuning_variance = np.array([utils.bf_sum_var(
             weights=self.B[:, neuron],
             centers=self.bf_centers,
             scale=self.bf_scale,
             limits=(0, 1)) for neuron in range(self.N)])
-        self.Psi = tuning_variance
+        # noise variances are scaled by signal-to-noise ratio
+        noise_variance = tuning_variance / self.noise_kwargs['snr']
+        # get noise covariance matrix
+        noise_cov = utils.corr2cov(noise_corr, noise_variance)
+        lamb, W = np.linalg.eigh(noise_cov)
+        # break down eigenvectors into shared and private components
+        self.L_nt = np.diag(np.sqrt(lamb[-self.K:])) @ W[:, -self.K:].T
+        self.Psi_nt = np.diag(noise_cov) - np.diag(self.L_nt.T @ self.L_nt)
+        return noise_corr, noise_cov
 
     def generate_samples(self, n_samples, bin_width=0.5, random_state=None):
+        """Generate samples from the triangular model.
+
+        Parameters
+        ----------
+        n_samples : int
+            The number of samples.
+
+        bin_width : float
+            Sets the bin width for the Poisson model.
+
+        random_state : RandomState or None
+            The RandomState instance to draw samples with. If None, the
+            RandomState instance of the class is used.
+
+        Returns
+        -------
+        stimuli : np.ndarray, shape (n_samples,)
+            The stimuli that generated the samples.
+
+        X : np.ndarray, shape (n_samples, M)
+            The tuning features for each stimulus.
+
+        Y : np.ndarray, shape (n_samples, N)
+            The non-target neural activity matrix.
+
+        y : np.ndarray, shape (n_samples, 1)
+            The target neural activity responses.
+        """
         if random_state is None:
             random_state = self.random_state
         else:
             random_state = check_random_state(random_state)
 
         if self.parameter_design == 'basis_functions':
+            # draw stimulus and tuning features
             stimuli = random_state.uniform(low=0, high=1, size=n_samples)
             X = utils.calculate_tuning_features(stimuli, self.bf_centers, self.bf_scale)
-            Z = random_state.normal(loc=0, scale=1.0, size=(n_samples, self.n_latent))
+            # draw latent activity
+            Z = random_state.normal(loc=0, scale=1.0, size=(n_samples, self.K))
 
             if self.model == 'linear':
                 # non-target private variability
@@ -243,13 +290,13 @@ class TriangularModel:
                                                                     scale=1.0,
                                                                     size=(n_samples, self.N))
                 # non-target neural activity
-                Y = np.dot(X, self.B) + np.dot(Z, self.L) + psi_nt
+                Y = np.dot(X, self.B) + np.dot(Z, self.L_nt) + psi_nt
                 # target private variability
-                psi_t = np.sqrt(self.Psi_t) * random_state.normal(loc=0,
-                                                                  scale=1.0,
-                                                                  size=n_samples)
+                # psi_t = np.sqrt(self.Psi_t) * random_state.normal(loc=0,
+                #                                                  scale=1.0,
+                #                                                  size=n_samples)
                 # target neural activity
-                y = np.dot(X, self.b) + np.dot(Y, self.a) + np.dot(Z, self.l) + psi_t
+                y = np.dot(X, self.b) + np.dot(Y, self.a)  # + np.dot(Z, self.l) + psi_t
             elif self.model == 'poisson':
                 # non-target responses
                 non_target_pre_exp = np.dot(X, self.B) + np.dot(Z, self.L)
@@ -294,34 +341,61 @@ class TriangularModel:
         self.A = self.A + Delta
         self.Bi = self.Bi - np.dot(self.Bj, Delta)
 
-    def plot_tuning_curves(self, fax=None, linewidth=1):
-        if fax is None:
-            fig, ax = plt.subplots(1, 1, figsize=(12, 6))
+    def plot_tuning_curves(self, neuron='all', fax=None, linewidth=1):
+        """Plots the tuning curve(s) of the neurons in the triangular model.
+
+        Parameters
+        ----------
+        neuron : string or array-like, default 'all'
+            The neurons to plot. If 'all', all neurons plotted. If 'non-target',
+            only non-target tuning curves are plotted. If 'target', only target
+            tuning curves are plotted. If array-like, contains the neurons
+            to plot directly.
+
+        fax : tuple of mpl.figure and mpl.axes, or None
+            The figure and axes. If None, a new set will be created.
+
+        linewidth : float
+            The widths of the plotted tuning curves.
+
+        Returns
+        -------
+        fax : tuple of mpl.figure and mpl.axes
+            The figure and axes, with tuning curves plotted.
+        """
+        fig, ax = utils.check_fax(fax, figsize=(12, 6))
+
+        # figure out which neurons need to be plotted
+        if neuron == 'all':
+            to_plot = np.arange(self.N + 1)
+        elif neuron == 'target':
+            to_plot = np.array([self.N])
+        elif neuron == 'non-target':
+            to_plot = np.arange(self.N)
+        elif isinstance(neuron, (list, tuple, np.ndarray)):
+            to_plot = neuron
         else:
-            fig, ax = fax
+            raise ValueError('Value type for neuron not supported.')
 
         if self.parameter_design == 'basis_functions':
-            stimuli, non_target_tuning = utils.calculate_tuning_curves(
-                B=self.B, bf_centers=self.bf_centers, bf_scale=self.bf_scale,
-            )
-            _, target_tuning = utils.calculate_tuning_curves(
-                B=self.b, bf_centers=self.bf_centers, bf_scale=self.bf_scale
-            )
-            # plot target responses
-            ax.plot(stimuli,
-                    target_tuning,
-                    color='r',
-                    linewidth=linewidth)
-            # plot coupled responses
-            for neuron in self.coupled_indices:
+            # get tuning curves for all neurons
+            stimuli, tuning_curves = utils.calculate_tuning_curves(
+                B=self.B_all,
+                bf_centers=self.bf_centers,
+                bf_scale=self.bf_scale)
+            # iterate over tuning curves to plot
+            for idx in to_plot:
+                # color of tuning curve depends on target, coupled, or non-coupled
+                if idx in self.coupled_indices:
+                    color = 'black'
+                elif idx in self.non_coupled_indices:
+                    color = 'gray'
+                elif idx == self.N:
+                    color = 'red'
+                # plot tuning curve
                 ax.plot(stimuli,
-                        non_target_tuning[:, neuron],
-                        color='black',
-                        linewidth=linewidth)
-            for neuron in self.non_coupled_indices:
-                ax.plot(stimuli,
-                        non_target_tuning[:, neuron],
-                        color='gray',
+                        tuning_curves[:, idx],
+                        color=color,
                         linewidth=linewidth)
         return fig, ax
 

neurobiases/utils.py
@@ -1,6 +1,34 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
 from scipy.special import erf
+
+
+def check_fax(fax=None, figsize=None):
+    """Checks an incoming set of axes, and creates new ones if needed.
+
+    Parameters
+    ----------
+    fax : tuple of mpl.figure and mpl.axes, or None
+        The figure and axes. If None, a new set will be created.
+
+    figsize : tuple or None
+        The figure size, if fax is None.
+
+    Returns
+    -------
+    fig, ax : mpl.figure and mpl.axes
+        The matplotlib axes objects.
+    """
+    # no axes provided
+    if fax is None:
+        # no figure size provided: use default
+        if figsize is None:
+            figsize = (10, 10)
+        fig, ax = plt.subplots(1, 1, figsize=figsize)
+    else:
+        fig, ax = fax
+    return fig, ax
 
 
 def copy_attribute_dict(attributes):
@@ -320,12 +348,74 @@ def calculate_pref_tuning(
 
 
 def noise_correlation_matrix(
-    tuning_prefs, corr_upper, L=1, corr_lower=0, circular_stim=None
+    tuning_prefs, corr_max, corr_min=0, L=1, circular_stim=None
 ):
+    """Create noise correlation matrix according to tuning preference.
+    Correlations fall off with exponential decay.
+
+    Parameters
+    ----------
+    tuning_prefs : np.ndarray, shape (n_neurons,)
+        The tuning preferences for each neuron.
+
+    corr_max : float
+        The maximum possible noise correlation.
+
+    corr_min : float
+        The minimum possible noise correlation.
+
+    L : float
+        The exponential decay constant. Lower values imply that correlations
+        fall off more steeply.
+
+    circular_stim : None or float
+        If None, stimulus is treated normally. Otherwise, stimulus is treated
+        circularly, with this value taking on the maximum value of the stimulus.
+
+    Returns
+    -------
+    corrs : np.ndarray, shape (n_neurons, n_neurons)
+        The correlation matrix.
+    """
+    # calculate tuning differences
     diffs = np.abs(np.subtract.outer(tuning_prefs, tuning_prefs))
+    # if stimulus is circular, modify differences
     if circular_stim is not None:
         thres = circular_stim / 2
         diffs[diffs > thres] = circular_stim - diffs[diffs > thres]
-
-    corrs = corr_lower + corr_upper * np.exp(-diffs / L)
+    # calculate correlation values
+    corrs = corr_min + corr_max * np.exp(-diffs / L)
+    # replace diagonal with ones
+    np.fill_diagonal(corrs, 1)
     return corrs
+
+
+def cov2corr(cov):
+    """Convert a covariance matrix to a correlation matrix."""
+    stdevs = np.sqrt(np.diag(cov))
+    outer = np.outer(stdevs, stdevs)
+    corr = cov / outer
+    return corr
+
+
+def corr2cov(corr, var):
+    """Converts a correlation matrix to a covariance matrix, given a set of
+    variances.
+
+    Parameters
+    ----------
+    corr : np.ndarray, shape (n_units, n_units)
+        The correlation matrix.
+
+    var : np.ndarray, shape (n_units,)
+        A vector of variances for the units in the correlation matrix.
+
+    Returns
+    -------
+    cov : np.ndarray, shape (n_units, n_units)
+        The covariance matrix.
+    """
+    stdevs = np.sqrt(var)
+    outer = np.outer(stdevs, stdevs)
+    cov = corr * outer
+    return cov

tests/test_utils.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 from neurobiases import utils
-from numpy.testing import assert_allclose
+from numpy.testing import (assert_allclose,
+                           assert_equal)
 
 
 def test_bf_mean_var():
@@ -14,7 +15,7 @@ def test_bf_mean_var():
     # calculate basis function output for stimuli
     features = utils.calculate_tuning_features(
         stimuli=stimuli,
-        bf_center=center,
+        bf_centers=center,
         bf_scale=scale)
     # calculate true and empirical mean
     mean_true = utils.bf_mean(center, scale, limits=(0, 1))
@@ -37,7 +38,7 @@ def test_bf_pairwise_cov():
     # calculate basis function output for stimuli
     features = utils.calculate_tuning_features(
         stimuli=stimuli,
-        bf_center=centers,
+        bf_centers=centers,
         bf_scale=scale)
     # calculate true and empirical covariances
     covar_true = utils.bf_pairwise_cov(centers, scale, limits=(0, 1))
@@ -56,12 +57,12 @@ def test_bf_cov():
     # calculate basis function output for stimuli
     features = utils.calculate_tuning_features(
         stimuli=stimuli,
-        bf_center=centers,
+        bf_centers=centers,
         bf_scale=scale)
     # calculate true and empirical covariances
     cov_true = utils.bf_cov(centers, scale, limits=(0, 1))
     cov_empirical = np.cov(features.T)
-    assert_allclose(cov_true, cov_empirical, rtol=0.1)
+    assert_allclose(cov_true, cov_empirical, rtol=0.2)
 
 
 def test_bf_sum_var():
@@ -75,10 +76,44 @@ def test_bf_sum_var():
     # calculate basis function output for stimuli
     features = utils.calculate_tuning_features(
         stimuli=stimuli,
-        bf_center=centers,
+        bf_centers=centers,
         bf_scale=scale)
     # calculate true and empirical weights
     var_true = utils.bf_sum_var(weights, centers, scale, limits=(0, 1))
     var_empirical = np.var(np.dot(features, weights))
     # assert empirical variance
     assert_allclose(var_true, var_empirical, rtol=0.1)
+
+
+def test_noise_corr_matrix():
+    """Tests whether the noise correlation matrix is generated correctly."""
+    tuning_prefs = np.array([0.25, 0.50, 0.75, 1.0])
+    n_neurons = tuning_prefs.size
+    corr_max = 1.0
+    L = 0.25
+
+    # test without circular stimulus
+    noise_corrs = utils.noise_correlation_matrix(tuning_prefs=tuning_prefs,
+                                                 corr_max=corr_max,
+                                                 L=L)
+    # test diagonal of noise correlation matrix
+    assert_equal(np.diag(noise_corrs), np.ones(n_neurons))
+    # test that off-diagonal terms are equal
+    assert np.unique(np.diag(noise_corrs, k=1)).size == 1
+    # test with circular stimulus
+    noise_corrs = utils.noise_correlation_matrix(tuning_prefs=tuning_prefs,
+                                                 corr_max=corr_max,
+                                                 L=L,
+                                                 circular_stim=1)
+    reverse_diagonal = np.diag(np.flip(noise_corrs, axis=1))
+    assert np.unique(reverse_diagonal).size == 1
+
+
+def test_cov_corr_functions():
+    """Test the covariance to correlation functions."""
+    X = np.random.normal(loc=0, scale=0.25, size=(100, 50))
+    cov = np.cov(X, rowvar=False)
+    corr = np.corrcoef(X, rowvar=False)
+
+    assert_allclose(corr, utils.cov2corr(cov))
+    assert_allclose(cov, utils.corr2cov(corr, np.diag(cov)))