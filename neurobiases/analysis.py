bc804875b169f4a3bd1c36a2a6c9210310bbabf2
Pratik Sachdeva <pratik.sachdeva23@gmail.com>
2021-04-02 18:41:19 -0700

    analysis scripts

--- neurobiases/analysis.py
+++ neurobiases/analysis.py
@@ -1,15 +1,77 @@
 import neuropacks as packs
 import numpy as np
 
+from sklearn.linear_model import LassoCV, LinearRegression
+
 
 def bic_linear(y_true, y_pred, n_features):
+    """Calculates the BIC of a linear model.
+
+    Parameters
+    ----------
+    y_true : np.ndarray
+        The true response variable values.
+    y_pred : np.ndarray
+        The predicted response variable values.
+    n_features : int
+        The number of features.
+
+    Returns
+    -------
+    bic : float
+        The Bayesian information criterion.
+    """
     n_samples = y_true.size
     rss_mean = np.mean((y_true - y_pred)**2)
     bic = n_samples * np.log(rss_mean) + n_features * np.log(n_samples)
     return bic
 
 
+def get_fitter(**kwargs):
+    """Creates a scikit-learn fitting object using keyword arguments."""
+    fit = kwargs.get('fit')
+    # Fit an ordinary linear regression
+    if fit == 'ols':
+        fitter = LinearRegression(
+            fit_intercept=kwargs.get('fit_intercept', True),
+            normalize=kwargs.get('normalize', False))
+    # Fit a cross-validated lasso regression
+    elif fit == 'lasso':
+        fitter = LassoCV(
+            eps=kwargs.get('eps', 1e-3),
+            n_alphas=kwargs.get('n_alphas', 100),
+            fit_intercept=kwargs.get('fit_intercept', True),
+            normalize=kwargs.get('normalize', False),
+            max_iter=kwargs.get('max_iter', 5000),
+            tol=kwargs.get('tol', 1e-4),
+            cv=kwargs.get('cv', 5))
+    return fitter
+
+
 def get_modulations_and_preferences(form, coefs, **kwargs):
+    """Calculates the tuning modulations and preferences for a set of tuning
+    coefficients.
+
+    Additional kwargs detail the tuning curve structure.
+
+    Parameters
+    ----------
+    form : str
+        The form of the tuning coefficients. Currently supports 'cosine2'
+        (cosine basis functions with a period of pi), and 'gbf', or gaussian
+        basis functions.
+    coefs : np.ndarray
+        The tuning coefficients, with first dimension spanning different
+        tuning curves.
+
+    Returns
+    -------
+    modulations : np.ndarray
+        The tuning modulation for each tuning coefficient set.
+    preferences : np.ndarray
+        The tuning preference for each tuning coefficient set.
+    """
+    # Cosine basis functions
     if form == 'cosine2':
         c1 = coefs[..., 0]
         c2 = coefs[..., 1]
@@ -21,7 +83,9 @@ def get_modulations_and_preferences(form, coefs, **kwargs):
         preferences = (preferences / 2) % 180
         # Calculate modulation
         modulations = 2 * (c2 - c1)/(np.sin(preferences_rad) - np.cos(preferences_rad))
+    # Gaussian basis functions
     elif form == 'gbf':
+        # Determine the tuning curve structure
         inputs = kwargs.get('inputs')
         means = kwargs.get('means')
         var = kwargs.get('var')
@@ -67,28 +131,62 @@ def import_data(dataset, data_path, n_gaussians=7, transform='square_root'):
 
 
 def tuning_fit(X, Y, fitter, X_test=None, Y_test=None):
+    """Perform a tuning fit across a set of neurons.
+
+    Parameters
+    ----------
+    X : np.ndarray, shape (n_samples, n_features)
+        The design matrix.
+    Y : np.ndarray, shape (n_samples, n_units)
+        The response matrix, or neural activity matrix.
+    fitter : object
+        The fitting object.
+    X_test : np.ndarray, shape (n_samples, n_features), optional
+        Test design matrix for calculating generalization performance. If
+        None, no test evaluation is performed.
+    Y_test : np.ndarray, shape (n_samples, n_features), optional
+        Test response matrix for calculating generalization performance. If
+        None, no test evaluation is performed.
+
+    Returns
+    -------
+    intercepts : np.ndarray, shape (n_units,)
+        The intercept for each fit.
+    tuning_coefs : np.ndarray, shape (n_units, n_features)
+        The tuning coefficient for each fit.
+    train_scores : np.ndarray, shape (n_units,)
+        The score of the model on training data.
+    test_scores : np.ndarray, shape (n_units,)
+        The score of the model on test data. Only returned if X_test and
+        Y_test are provided.
+    bics : np.ndarray, shape (n_units,)
+        The BIC evaluated on the training data.
+    """
     n_features = X.shape[1]
-    n_neurons = Y.shape[1]
+    n_units = Y.shape[1]
     # Create storage arrays
-    intercepts = np.zeros(n_neurons)
-    tuning_coefs = np.zeros((n_neurons, n_features))
-    train_scores = np.zeros(n_neurons)
+    intercepts = np.zeros(n_units)
+    tuning_coefs = np.zeros((n_units, n_features))
+    train_scores = np.zeros(n_units)
+    # Check if we need to provide test performance
     if (X_test is not None) and (Y_test is not None):
         test_set = True
         test_scores = np.zeros_like(train_scores)
     bics = np.zeros_like(train_scores)
 
-    for neuron in range(n_neurons):
+    # Perform fits across units
+    for unit in range(n_units):
         # Run tuning fit
-        fitter.fit(X, Y[:, neuron])
+        fitter.fit(X, Y[:, unit])
         # Store coefficients
-        intercepts[neuron] = fitter.intercept_
-        tuning_coefs[neuron] = fitter.coef_
+        intercepts[unit] = fitter.intercept_
+        tuning_coefs[unit] = fitter.coef_
         # Evaluate model
-        train_scores[neuron] = fitter.score(X, Y[:, neuron])
+        train_scores[unit] = fitter.score(X, Y[:, unit])
         if test_set:
-            test_scores[neuron] = fitter.score(X_test, Y_test[:, neuron])
-        bics[neuron] = bic_linear(Y[:, neuron], fitter.predict(X))
+            test_scores[unit] = fitter.score(X_test, Y_test[:, unit])
+        n_features = 1 + np.count_nonzero(fitter.coef_)
+        bics[unit] = bic_linear(Y[:, unit], fitter.predict(X), n_features)
 
     if test_set:
         return intercepts, tuning_coefs, train_scores, test_scores, bics
--- scripts/neural_analysis.py
+++ scripts/neural_analysis.py
@@ -15,20 +15,25 @@ def main(args):
 
     model = args.model
     dataset = args.dataset
+    data_path = args.data_path
+    write_path = args.write_path
+    write_group = args.write_group
     n_folds = args.n_folds
-    standardize = args.standardize
-    verbose = args.verbose
+    cv_verbose = args.cv_verbose
+
+    fitter = analysis.get_fitter(**vars(args))
 
     # Get dataset
     X, Y, class_labels, pack = analysis.import_data(
         dataset=dataset,
-        data_path=args.data_path,
+        data_path=data_path,
         n_gaussians=args.n_gaussians,
         transform=args.transform)
-
     # Clear out empty units
     Y = Y[:, np.argwhere(Y.sum(axis=0) != 0).ravel()]
-    n_targets = Y.shape[1]
+    n_tuning_features = X.shape[1]
+    n_coupling_features = Y.shape[1] - 1
+    n_units = Y.shape[1]
 
     # Create stratified folds
     skfolds = StratifiedKFold(
@@ -39,24 +44,22 @@ def main(args):
     test_folds = {}
 
     # Create storage arrays
-    intercepts = np.zeros((n_folds, n_targets))
-    if model == 'tuning':
-        tuning_coefs = np.zeros((n_folds, n_targets, n_features))
-    elif model == 'coupling':
-        coupling_coefs = np.zeros((n_folds, n_targets, n_targets - 1))
-    elif model == 'tc':
-        tuning_coefs = np.zeros((n_folds, n_targets, n_features))
-        coupling_coefs = np.zeros((n_folds, n_targets, n_targets - 1))
-
-    r2s_train = np.zeros((n_folds, n_targets))
-    r2s_test = np.zeros_like(r2s_train)
-    bics = np.zeros(r2s_train)
+    intercepts = np.zeros((n_folds, n_units))
+    if model == 't':
+        tuning_coefs = np.zeros((n_folds, n_units, n_tuning_features))
+        modulations = np.zeros((n_folds, n_units))
+        preferences = np.zeros((n_folds, n_units))
+    elif model == 'c':
+        coupling_coefs = np.zeros((n_folds, n_units, n_coupling_features))
+    scores_train = np.zeros((n_folds, n_units))
+    scores_test = np.zeros_like(scores_train)
+    bics = np.zeros_like(scores_train)
 
     # Outer loop: iterate over cross-validation folds
     for fold_idx, (train_idx, test_idx) in enumerate(
         skfolds.split(y=class_labels, X=class_labels)
     ):
-        if verbose:
+        if cv_verbose:
             print(f'Fold {fold_idx}', flush=True)
         # Save train and test indices
         train_folds[f'fold_{fold_idx}'] = train_idx
@@ -67,12 +70,73 @@ def main(args):
         X_test = X[test_idx, :]
         Y_test = Y[test_idx, :]
 
-        if model == 'tuning':
+        # Perform the fit across all units
+        if model == 't':
             (intercepts[fold_idx], tuning_coefs[fold_idx],
-             train_scores[fold_idx], test_scores[fold_idx], bics[fold_idx]) = \
+             scores_train[fold_idx], scores_test[fold_idx], bics[fold_idx]) = \
                 analysis.tuning_fit(
                     X=X_train,
                     Y=Y_train,
                     fitter=fitter,
                     X_test=X_test,
                     Y_test=Y_test)
+            # Calculate modulation and preferences
+            modulations[fold_idx], preferences[fold_idx] = \
+                analysis.get_modulations_and_preferences(
+                    form='',
+                    coefs=tuning_coefs[fold_idx],
+                    
+                )
+
+        elif model == 'c':
+            (intercepts[fold_idx], coupling_coefs[fold_idx],
+             scores_train[fold_idx], scores_test[fold_idx], bics[fold_idx]) = \
+                analysis.tuning_fit(
+                    X=X_train,
+                    Y=Y_train,
+                    fitter=fitter,
+                    X_test=X_test,
+                    Y_test=Y_test)
+
+    # Write results to file
+    with h5py.File(write_path, 'a') as results:
+        group = results.create_group(write_group)
+        group['intercepts'] = intercepts
+        if model == 't':
+            group['tuning_coefs'] = tuning_coefs
+        elif model == 'c':
+            group['coupling_coefs'] = coupling_coefs
+        group['scores_train'] = scores_train
+        group['scores_test'] = scores_test
+        group['bic'] = bics
+        group.attrs['model'] = model
+        group.attrs['dataset'] = dataset
+        group.attrs['n_folds'] = n_folds
+        group.attrs['fitter'] = args.fit
+        # Write train and test indices
+        train_group = group.create_group('train_idx')
+        for key, val in train_folds.items():
+            train_group[key] = val
+        test_group = group.create_group('test_idx')
+        for key, val in test_folds.items():
+            test_group[key] = val
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Run a tuning or coupling fit on data.')
+    parser.add_argument('--model', choices=['t', 'c'])
+    parser.add_argument('--dataset', choices=['pvc11', 'ecog'])
+    parser.add_argument('--data_path')
+    parser.add_argument('--write_path')
+    parser.add_argument('--write_group')
+    parser.add_argument('--fit', choices=['ols', 'lasso'])
+    parser.add_argument('--n_folds', type=int, default=5)
+    parser.add_argument('--rng', type=int, default=-1)
+    # Data arguments
+    parser.add_argument('--n_gaussians', type=int, default=7)
+    parser.add_argument('--transform', default=None)
+    # Other arguments
+    parser.add_argument('--cv_verbose', action='store_true')
+    args = parser.parse_args()
+
+    main(args)
